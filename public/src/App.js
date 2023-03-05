24dd2610cb96f63de7752b7575385402df159831
Vinay Singhal <vinay.singhal@netenrich.com>
2023-03-05 21:31:38 +0530

    stlying for register page done

--- public/src/App.js
+++ public/src/App.js
@@ -1,7 +1,16 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/login';
+import Chat from './pages/chat';
+import Register from './pages/register';
+
 export default function App() {
   return (
-  <BrowserRouter></BrowserRouter>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>)
 }
